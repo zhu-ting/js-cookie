5d2ec415d69d4960cdf095b29887222ea8c40dd3
Klaus Hartl <klaus.hartl@gmail.com>
2019-12-08 20:16:24 +0100

    Extract converter into its own module

--- src/js.cookie.mjs
+++ src/js.cookie.mjs
@@ -1,3 +1,5 @@
+import rfc6265Converter from './rfc6265'
+
 function extend () {
   var result = {}
   for (var i = 0; i < arguments.length; i++) {
@@ -7,18 +9,6 @@ function extend () {
     }
   }
   return result
-}
-
-var rfc6265Converter = {
-  read: function (value) {
-    return value.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent)
-  },
-  write: function (value) {
-    return encodeURIComponent(value).replace(
-      /%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,
-      decodeURIComponent
-    )
-  }
 }
 
 function init (converter, defaultAttributes) {
---
+++ src/rfc6265.mjs
@@ -0,0 +1,11 @@
+export default {
+  read: function (value) {
+    return value.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent)
+  },
+  write: function (value) {
+    return encodeURIComponent(value).replace(
+      /%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,
+      decodeURIComponent
+    )
+  }
+}
